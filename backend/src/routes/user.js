e7b06a3cb5b616a290f66f12007bf56351e0c62e
Rithvik <1017rithvik@gmail.com>
2025-06-22 19:05:27 -0400

    user connections api

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -3,20 +3,21 @@ const { userAuth } = require("../middlewares/auth");
 const { ConnectionRequests } = require("../models/connectionRequests");
 const userRouter = express.Router();
 
+const userPopulateFeilds = [
+  "firstName",
+  "lastName",
+  "gender",
+  "age",
+  "about",
+  "photoUrl",
+];
 userRouter.get("/user/requests/received", userAuth, async (req, res) => {
   try {
     const loggedInUser = req.user;
     const connectionRequests = await ConnectionRequests.find({
       toUserId: loggedInUser._id,
       status: "interested",
-    }).populate("fromUserId", [
-      "firstName",
-      "lastName",
-      "photoUrl",
-      "age",
-      "about",
-      "gender",
-    ]);
+    }).populate("fromUserId", userPopulateFeilds);
     if (connectionRequests.length === 0) {
       throw new Error("No connection requests found.");
     }
@@ -31,6 +32,28 @@ userRouter.get("/user/requests/received", userAuth, async (req, res) => {
   }
 });
 
+userRouter.get("/user/connections", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+    const findConnections = await ConnectionRequests.find({
+      $or: [
+        { toUserId: loggedInUser._id, status: "accepted" },
+        { fromUserId: loggedInUser._id, status: "accepted" },
+      ],
+    }).populate("fromUserId", userPopulateFeilds);
+    if (findConnections.length === 0) {
+      throw new Error("No connection requests found.");
+    }
+    const data = findConnections.map((connection) => connection.fromUserId);
+    res.status(200).json({
+      data: data,
+    });
+  } catch (error) {
+    res.status(401).json({
+      message: "Error occured",
+    });
+  }
+});
 module.exports = {
   userRouter,
 };
