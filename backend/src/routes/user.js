b3309f79829b908d67566a06c0a3c58d65bb7db7
Rithvik <1017rithvik@gmail.com>
2025-06-23 12:29:32 -0400

    feed api and frontend setup

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { userAuth } = require("../middlewares/auth");
 const { ConnectionRequests } = require("../models/connectionRequests");
+const { User } = require("../models/user");
 const userRouter = express.Router();
 
 const userPopulateFeilds = [
@@ -61,6 +62,72 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
     });
   }
 });
+
+userRouter.get("/feed", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+
+    // request pagination
+    const page = req.query.page || 1;
+    // limit is the number of users to fetch
+    const limit = req.query.limit || 10;
+    limit = limit > 50 ? 50 : limit;
+
+    // skip is the number of users to skip
+    const skip = (page - 1) * limit;
+
+    if (!page || !limit) {
+      throw new Error("Page and limit are required.");
+    }
+
+    // get all the connections of the logged in user
+    const connectionRequests = await ConnectionRequests.find({
+      $or: [
+        {
+          fromUserId: loggedInUser._id,
+        },
+        { toUserId: loggedInUser._id },
+      ],
+    }).select(["fromUserId", "toUserId"]);
+
+    // hide users from feed that are already in the connections of the logged in user
+    const hideUsersFromFeed = new Set();
+
+    connectionRequests.forEach((connection) => {
+      hideUsersFromFeed.add(connection.fromUserId);
+      hideUsersFromFeed.add(connection.toUserId);
+    });
+
+    // get all the users that are not in the connections of the logged in user
+    const feedUsers = await User.find({
+      $and: [
+        {
+          _id: {
+            $nin: Array.from(hideUsersFromFeed),
+          },
+        },
+        {
+          _id: {
+            $ne: loggedInUser._id,
+          },
+        },
+      ],
+    })
+      .select(userPopulateFeilds)
+      .skip(skip)
+      .limit(limit);
+
+    res.status(200).json({
+      message: "Users fetched succesfully",
+      data: feedUsers,
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: error.message,
+    });
+  }
+});
+
 module.exports = {
   userRouter,
 };
